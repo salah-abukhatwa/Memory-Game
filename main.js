bdbd1da599c9b05d9fd0490989dec4773cf9ed31
salah-abukhatwa <salah.abukhatwa@gmail.com>
2023-04-05 14:33:05 +0300

    add Order , js code

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 document.querySelector(".control-buttons span").onclick = function () {
     let yourName = prompt('What is Your Name ?')
 
-    if (yourName == null || yourName =="" ) {
+    if (yourName == null || yourName == "") {
         document.querySelector(".name span").innerHTML = 'unkown';
     } else {
         document.querySelector(".name span").innerHTML = yourName;
     }
 
     document.querySelector(".control-buttons").remove();
-}
+};
+let duration = 1000;
+let blocksContainer = document.querySelector('.memore-game-blocks');
+let blocks = Array.from(blocksContainer.children);
+
+let orderRange = [...Array(blocks.length).keys()];
+
+// Add Order Css Property To Game Block
+blocks.forEach((block , index) => {
+
+    block.style.order = orderRange[index];
+   
+});
